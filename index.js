1866c13275c11b132cc12e6d2391c29bf333b066
KeetonMartin <klmartin@haverford.edu>
2021-02-02 16:27:22 -0500

    fix paren

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ express()
   .get('/menu/brynmawr/todayMenu', (req, res) => res.send(menuBrynMawrTodayMenu()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+menuBrynMawrTodayMenu = () => {
+
+  let dummyJSON = {
+    "messages": [
+      {"text": "February 3, 2021"},
+      {"text": "Lunch"},
+      {"text": "Fire Roasted Tomato Soup, Grilled American Cheese Sandwich*, Buffalo Tofu Wings V, Sundried Tomato, Artichoke & Swiss Frittata, Turkey Bacon, Shoestring Fries*, Chefs Choice Vegetabl, Potato Chips, Vegan Vanilla Cupcakes , Chocolate Chip Cookies*"},
+      {"text": "Dinner"},
+      {"text": "Louisiana Spiced Tofu V, Cajun Marinated Chicken Breast, Seasoned Baby Carrots, Dirty Rice V, Garlic Bread, Split-Top Dinner Roll*, Pasta Bar : Spaghetti, Marinara Sauce, Meat Sauce, Deep Chocolate Cake V, Lemon Cake*"}
+    ]
+    };
+    
+  return dummyJSON;
+}
+
 menuHaverfordTodayMenu = () => {
 
   let dummyJSON = {
@@ -26,26 +41,10 @@ menuHaverfordTodayMenu = () => {
       {"text": "Dinner"},
       {"text": "Grilled Flank Steak Chimichurri, Cauliflower Rice Burrito Skillet, Mexican Corn, Chipotle Roasted Potatoes, Grilled Chicken Breast, Pasta & Sauce, Pizza"}
     ]
-    }
+    };
     
   return dummyJSON;
 }
-
-menuBrynMawrTodayMenu() = () => {
-
-  let dummyJSON = {
-    "messages": [
-      {"text": "February 3, 2021"},
-      {"text": "Lunch"},
-      {"text": "Fire Roasted Tomato Soup, Grilled American Cheese Sandwich*, Buffalo Tofu Wings V, Sundried Tomato, Artichoke & Swiss Frittata, Turkey Bacon, Shoestring Fries*, Chefs Choice Vegetabl, Potato Chips, Vegan Vanilla Cupcakes , Chocolate Chip Cookies*"},
-      {"text": "Dinner"},
-      {"text": "Louisiana Spiced Tofu V, Cajun Marinated Chicken Breast, Seasoned Baby Carrots, Dirty Rice V, Garlic Bread, Split-Top Dinner Roll*, Pasta Bar : Spaghetti, Marinara Sauce, Meat Sauce, Deep Chocolate Cake V, Lemon Cake*"}
-    ]
-    }
-    
-  return dummyJSON;
-}
-  
 
 getLunch = () => {
 
