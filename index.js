08046d9e0d5a234e74b3fb53353b92f6475c9dc2
KeetonMartin <klmartin@haverford.edu>
2021-02-02 13:41:53 -0500

    Adding dummy JSON

--- index.js
+++ index.js
@@ -9,4 +9,45 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => res.send(showTimes()))
+  .get('/lunch', (req, res) => res.send(getLunch()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+getLunch = () => {
+
+  let dummyJSON = {
+    "user1" : {
+       "name" : "mahesh",
+       "password" : "password1",
+       "profession" : "teacher",
+       "id": 1
+    },
+    
+    "user2" : {
+       "name" : "suresh",
+       "password" : "password2",
+       "profession" : "librarian",
+       "id": 2
+    },
+    
+    "user3" : {
+       "name" : "ramesh",
+       "password" : "password3",
+       "profession" : "clerk",
+       "id": 3
+    }
+ }
+
+  return dummyJSON;
+}
+  
+
+showTimes = () => {
+  let result = '';
+  const times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++) {
+    result += i + ' ';
+  }
+  return result;
+}
