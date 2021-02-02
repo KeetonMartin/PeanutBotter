d2dae27a4efd238b11f37fcc0f60d4984737e00c
KeetonMartin <klmartin@haverford.edu>
2021-02-02 15:21:14 -0500

    Changing dummy JSON to be presentable in messenger

--- index.js
+++ index.js
@@ -17,28 +17,12 @@ express()
 getLunch = () => {
 
   let dummyJSON = {
-    "user1" : {
-       "name" : "mahesh",
-       "password" : "password1",
-       "profession" : "teacher",
-       "id": 1
-    },
-    
-    "user2" : {
-       "name" : "suresh",
-       "password" : "password2",
-       "profession" : "librarian",
-       "id": 2
-    },
-    
-    "user3" : {
-       "name" : "ramesh",
-       "password" : "password3",
-       "profession" : "clerk",
-       "id": 3
-    }
- }
-
+    "messages": [
+      {"text": "Welcome to the Chatfuel Rockets!"},
+      {"text": "What are you up to?"}
+    ]
+   }
+   
   return dummyJSON;
 }
   
