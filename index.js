22edaf79ae664f1b248c0ad25c5272552f9ee5c5
KeetonMartin <klmartin@haverford.edu>
2021-02-02 16:17:00 -0500

    Bryn mawr today menu

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ express()
   .get('/times', (req, res) => res.send(showTimes()))
   .get('/lunch', (req, res) => res.send(getLunch()))
   .get('/menu/haverford/todayMenu', (req, res) => res.send(menuHaverfordTodayMenu()))
+  .get('/menu/brynmawr/todayMenu', (req, res) => res.send(menuBrynMawrTodayMenu()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 menuHaverfordTodayMenu = () => {
@@ -24,6 +25,21 @@ menuHaverfordTodayMenu = () => {
       {"text": "11:30 AM, Grilled Turkey & Swiss Sundried Tomato Melt, Singapore Street Noodles , Roasted Red Potatoes , Roasted Vegetables, Pizza"},
       {"text": "Dinner"},
       {"text": "Grilled Flank Steak Chimichurri, Cauliflower Rice Burrito Skillet, Mexican Corn, Chipotle Roasted Potatoes, Grilled Chicken Breast, Pasta & Sauce, Pizza"}
+    ]
+    }
+    
+  return dummyJSON;
+}
+
+menuBrynMawrTodayMenu() = () => {
+
+  let dummyJSON = {
+    "messages": [
+      {"text": "February 3, 2021"},
+      {"text": "Lunch"},
+      {"text": "Fire Roasted Tomato Soup, Grilled American Cheese Sandwich*, Buffalo Tofu Wings V, Sundried Tomato, Artichoke & Swiss Frittata, Turkey Bacon, Shoestring Fries*, Chefs Choice Vegetabl, Potato Chips, Vegan Vanilla Cupcakes , Chocolate Chip Cookies*"},
+      {"text": "Dinner"},
+      {"text": "Louisiana Spiced Tofu V, Cajun Marinated Chicken Breast, Seasoned Baby Carrots, Dirty Rice V, Garlic Bread, Split-Top Dinner Roll*, Pasta Bar : Spaghetti, Marinara Sauce, Meat Sauce, Deep Chocolate Cake V, Lemon Cake*"}
     ]
     }
     
