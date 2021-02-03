2abedb4a9f1798e7e8ab0da11f63cf08151b24a0
ikhovryak <ikhovryak@haverford.edu>
2021-02-02 21:15:27 -0500

    minor bug fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cool = require('cool-ascii-faces');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 express()
